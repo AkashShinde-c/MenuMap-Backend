6a07e8de43274dc5627a4a29da92cf94526874b7
AkashShinde-c <shindeakashak47@gmail.com>
2023-12-18 21:26:17 +0530

    Updated the get image url api

--- src/controllers/get_image_url.js
+++ src/controllers/get_image_url.js
@@ -1,14 +1,14 @@
 const { getObjectURL } = require("../utils/awsurlutils");
 
-const get_image_url = async(req, res)=>{
-    try {
-        const {img_name} = req.body;
-    
-        const url = await getObjectURL(`uploads/user-uploads/${img_name}.jpeg`);
-        res.status(200).json({url:url});
-    } catch (error) {
-        res.status(500).json({error:"An error occured"});
-    }
-}
+const get_image_url = async (req, res) => {
+  try {
+    const img_name  = req.query.img_name;
+    const url = await getObjectURL(`uploads/user-uploads/${img_name}.jpeg`);
+    res.status(200).json({ url: url });
+  } catch (error) {
+    res.status(500).json({ error: "An error occured" });
+    console.log(error)
+  }
+};
 
 module.exports = get_image_url;
--- src/controllers/get_menu_data.js
+++ src/controllers/get_menu_data.js
@@ -1,15 +1,10 @@
-const User = require('../models/userModel')
+const User = require("../models/userModel");
 
-const get_menu_data = async(req, res)=>{
-    try {
-        
-
-        const menu_data = await User.find();
-        console.log(menu_data)
-        res.status(200).json({menu_data})
-    } catch (error) {
-        
-    }
-}
+const get_menu_data = async (req, res) => {
+  try {
+    const menu_data = await User.find();
+    res.status(200).json({ menu_data });
+  } catch (error) {}
+};
 
 module.exports = get_menu_data;
